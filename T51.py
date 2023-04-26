35e3932cd8c2f9da18a533bfa92ed93d4c19cfe0
linn823 <278577763@qq.com>
2023-04-26 03:45:22 -0400

    busquedaProfundidadaleatoria

--- T51.py
+++ T51.py
@@ -1,18 +1,59 @@
 from dataclasses import dataclass
-#comentario
-def BúsquedaProfundidad (escogiendo un sucesor al azar):
-	
-	
-#comentario
-def BúsquedaCostoUniforme:
-	
-#comentario
-def BúsquedaGreedy:
-	
-#comentario
-def A*:
 
-#nodo
+class nodoP():
+  nombre = str
+  valor = int
+  estado = bool
+  profundidad = int
+  vecinos = []
+
+  def __init__(self,nombre,valor):    
+    self.nombre = nombre
+    self.costo = valor
+    self.estado = False
+    
+  
+class Búsquedaenprofundidad:
+	pila=[]
+	nodoInicial=nodoP(Nodo_inicial,0)
+	def AddPila(nodo):
+		pila.append(nodo)
+
+	def PopPila():
+		pila.pop(0)
+		#print(l.pop(0))elimina  primer elemento de Lista l.
+		# a.pop() quita y retorna el último elemento de la lista.
+
+	def generarArbol():
+
+	def buscar(nodoActual,nodoObjetivo):
+		pila.append(nodoActual)
+		if nodoActual == nodoObjetivo:
+			return pila
+      
+		nodosSucesores=nodo_actual.vecinos
+
+		if len(nodosSucesores) > 0 :
+			nodoAleatorio = random.choice(nodosSucesores)
+		if nodoALeatorio.estado != False:
+			nuevoNodo = buscar(nodoAleatorio,nodoObjetivo)
+			pila.append(nuevoNodo)
+		if nuevoNodo is None
+			pila.pop(0)
+			return None
+		else:
+			return nuevoNodo
+      
+class Búsqueda por costo uniforme(nodo):
+
+
+class Búsqueda greedy(nodo):
+#comentario
+
+class AEstrella(nodo):
+#comentario
+
+#nodoXD
 clase node:
 	name:str
 	cost:int
---
+++ T52.py
@@ -0,0 +1,8 @@
+- ¿Qué puede decir de la comparación entre los métodos implementados? En concreto, los que
+encontraron la solución óptima, a qué se debió? Y los que no, ¿por qué no la encontraron?
+
+
+
+- De los métodos vistos en clase, ¿hay alguno que NO retorne una solución (es decir, que se
+mantenga realizando búsqueda ad infinitum) en este problema? Si es así, cuál? Y si no, por
+qué no?
